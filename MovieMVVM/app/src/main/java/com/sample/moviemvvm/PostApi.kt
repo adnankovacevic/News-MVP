1523fa5335c7efbd5c06ac72cc25c434991f7295
kovacevica9 <kovacevic9adnan@gmail.com>
2022-01-25 20:26:17 +0100

    Delete MovieMVVM directory

--- MovieMVVM/app/src/main/java/com/sample/moviemvvm/PostApi.kt
+++
@@ -1,9 +0,0 @@
-package com.sample.moviemvvm
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface PostApi {
-    @GET("posts")
-    suspend fun getPosts(): Response<List<Post>>
-}
--- MovieMVVM/app/src/main/java/com/sample/moviemvvm/PostRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.sample.moviemvvm
-
-import javax.inject.Inject
-
-class PostRepository @Inject constructor(private val postApi: PostApi) {
-
-    suspend fun getPosts() = postApi.getPosts()
-}
